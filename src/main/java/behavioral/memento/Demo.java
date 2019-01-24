10a61ebb88e993692b461e1a84afe6b2db68d065
Paxnamra <bialyfeniks4@gmail.com>
2019-01-24 13:09:14 +0100

    Memento | serialize/deserialize file

---
+++ src/main/java/behavioral/memento/Demo.java
@@ -0,0 +1,50 @@
+package behavioral.memento;
+
+import java.io.*;
+
+public class Demo {
+
+    private static Employee deserialize(){
+
+        Employee emp = null;
+        try {
+            FileInputStream fileIn = new FileInputStream("/tmp/employee/ser.");
+            ObjectInputStream in = new ObjectInputStream(fileIn);
+            emp = (Employee) in.readObject();
+            in.close();
+            fileIn.close();
+        } catch (ClassNotFoundException | IOException e) {
+            e.printStackTrace();
+        }
+        return emp;
+    }
+
+    private static void serialize(Employee emp) {
+
+        try {
+            FileOutputStream fileOut = new FileOutputStream("/tmp/employee/ser.");
+            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+            out.writeObject(emp);
+            out.close();
+            fileOut.close();
+            } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Employee emp = new Employee();
+        emp.setName("Jenny");
+        emp.setPhone("003-12-5568");
+        emp.setAddress("Wenus, unknown localization st 0/2");
+
+        serialize(emp);
+
+        Employee newEmp = deserialize();
+        System.out.println(newEmp.getName());
+
+    }
+}
+
+//TODO path localizaton error to fix
---
+++ src/main/java/behavioral/memento/Employee.java
@@ -0,0 +1,49 @@
+package behavioral.memento;
+
+import java.io.Serializable;
+
+//origginator
+@SuppressWarnings("serial")
+public class Employee implements Serializable {
+
+    private String name;
+    private String address;
+    private String phone;
+
+    public Employee() {
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Employee{");
+        sb.append("name='").append(name).append('\'');
+        sb.append(", address='").append(address).append('\'');
+        sb.append(", phone='").append(phone).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+}
