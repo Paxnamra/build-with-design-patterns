263d699659c2a61daf273efeea6d8d702103cd49
Paxnamra <bialyfeniks4@gmail.com>
2019-01-25 01:53:51 +0100

    State | Simple fan if/else ifs

---
+++ src/main/java/behavioral/state/Fan.java
@@ -0,0 +1,41 @@
+package behavioral.state;
+
+public class Fan {
+
+    final static int OFF = 0;
+    final static int LOW = 1;
+    final static int MED = 2;
+
+    int state = OFF;
+
+    public Fan() {
+    }
+
+    public void pullChain() {
+        if(state == OFF) {
+            System.out.println("Turning fan on to low");
+            state = LOW;
+        }
+        else if (state == LOW) {
+            System.out.println("Turning fan on to med");
+            state = MED;
+        }
+        else if (state == MED) {
+            System.out.println("Turning fan off");
+            state = OFF;
+        }
+    }
+
+    public String toString() {
+        if(state == OFF) {
+            return "Fan is off";
+        }
+        else if (state == LOW) {
+            return "Fan is on low";
+        }
+        else if (state == MED) {
+            return "Fan is on med";
+        }
+        return "Invalid state";
+    }
+}
---
+++ src/main/java/behavioral/state/Main.java
@@ -0,0 +1,15 @@
+package behavioral.state;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Fan fan = new Fan();
+        System.out.println(fan);
+
+        fan.pullChain();
+        System.out.println(fan);
+
+        fan.pullChain();
+        System.out.println(fan);
+    }
+}
