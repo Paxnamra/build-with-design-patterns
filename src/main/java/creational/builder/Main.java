e323329b6c10ab694451c29f779e516e87cd35dc
Paxnamra <bialyfeniks4@gmail.com>
2019-02-23 18:17:24 +0100

    Builder | FoodOrder

---
+++ src/main/java/creational/builder/Main.java
@@ -0,0 +1,17 @@
+package creational.builder;
+
+public class Main {
+    public static void main(String[] args) {
+
+        FoodOrder.Builder builder = new FoodOrder.Builder();
+        builder.bread("Wheat").condiments("Tomato").dressing("Vinegar").meat("Mozzarella");
+
+        FoodOrder foodOrder = builder.makeSandwitch();
+
+        System.out.println(foodOrder.getBread());
+        System.out.println(foodOrder.getCondiments());
+        System.out.println(foodOrder.getDressing());
+        System.out.println(foodOrder.getMeat());
+    }
+
+}
